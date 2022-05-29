45a3478f3b0bfad023f5157ce7d183294e835fa9
markd-hiyield <mark@hiyield.co.uk>
2022-05-29 14:17:00 +0100

    Added mapbox map

---
+++ components/Map.vue
@@ -0,0 +1,47 @@
+<template>
+    <div id="map"></div>
+</template>
+
+<script>
+  import mapboxgl from 'mapbox-gl'
+  import 'mapbox-gl/dist/mapbox-gl.css';
+  export default {
+    data(){
+      return{
+        access_token: 'pk.eyJ1IjoibWFya2Rhd3NvbjEiLCJhIjoiY2wzNzFvOTQwMXRoMjNpcDlld2FhZTNxcSJ9.gCW5I0cLw2Xq_-EKawtcmA',
+        map: {}
+      }
+    },
+    mounted(){
+      this.createMap()
+      // yourMarkersArray.map((marker) => {
+      // const LngLat = [marker.location.lng, marker.location.lat]
+      // const popup = marker.description
+      // new mapboxgl.Marker()
+      //     .setLngLat(LngLat)
+      //     .setPopup(popup)
+      //     .addTo(map) // Initialized above
+      // })
+    },
+    methods:{
+      createMap(){
+        mapboxgl.accessToken = this.access_token
+        this.map= new mapboxgl.Map({
+          container: 'map',
+          style: 'mapbox://styles/mapbox/streets-v11',
+          zoom: 10,
+          center: [-5.5376, 50.1188]
+        }),
+        this.map.addControl(new mapboxgl.NavigationControl())
+      }
+    }
+
+  }
+</script>
+
+<style>
+#map{
+  width:100%;
+  height:100vh;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,30 @@
 <template>
-  <Tutorial/>
+  <div class="w-full h-full">
+    <div class="flex mb-4">
+      <div class="w-1/2 bg-gray-400">
+        <Map/>
+      </div>
+      <div class="w-1/2 bg-gray-500">
+      </div>
+    </div>
+  </div> 
+
 </template>
 
 <script>
-export default {
-  name: 'IndexPage'
-}
+  import Map from '@/components/Map.vue'
+  import mapboxgl from 'mapbox-gl'
+  
+  export default {
+    name: 'Home',
+    layout: 'home',
+    auth: false,
+    components: {
+      Map
+  }
+  }
 </script>
+
+<style>
+
+</style>
